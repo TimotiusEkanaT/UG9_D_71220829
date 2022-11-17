c25c72d6ad68a3aee74184fa36c5bc1006667428
TimotiusEkanaT <117902158+TimotiusEkanaT@users.noreply.github.com>
2022-11-17 16:29:38 +0700

    Add files via upload

---
+++ 1_D_71220829.py
@@ -0,0 +1,5 @@
+print("kalkulator  lingkaran sederhana")
+jari=int(input("masukan jari-jari :"))
+rumus = 3.14*(jari**2)
+
+print(rumus)
---
+++ 3_D_71220829.py
@@ -0,0 +1,11 @@
+name=input("masukan nama Anda :")
+huaa=name.split(" ")
+ngarep=huaa[0]
+mburi=huaa[1]
+matk=input("Masukan mata Kuliah :")
+nahh=matk.split(" ")
+didepan=nahh[0]
+diblakang=nahh[1]
+hih=input("masukan grup :")
+print("Haloo!",mburi,",",ngarep)
+print(ngarep,"tergabung dalam kelas",didepan,diblakang,"pada grup",hih)
